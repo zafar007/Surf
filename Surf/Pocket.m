ec67bbfb346512326e9ab6c611e90c2717521325
Sapan Bhuta <sapanbhuta@gmail.com>
2014-06-28 16:39:31 -0500

    pocket #gb

--- Surf/Pocket.m
+++ Surf/Pocket.m
@@ -43,34 +43,33 @@
 {
     self.data = [NSMutableArray new];
 
-    [[PocketAPI sharedAPI] callAPIMethod:@"https://getpocket.com/v3/get"
+    [[PocketAPI sharedAPI] callAPIMethod:@"get"
                           withHTTPMethod:PocketAPIHTTPMethodPOST
-                               arguments:@{@"consumer_key":[[PocketAPI sharedAPI] consumerKey]}
+                               arguments:@{}
                                  handler:^(PocketAPI *api, NSString *apiMethod, NSDictionary *response, NSError *error)
     {
-        NSLog(@"RESPONSE: %@",response);
-
-        if (!error && !response)
+        if (!error && response)
         {
-            for (NSDictionary *hunt in response[@"data"])
-            {
-                NSString *productLink = hunt[@"url"];
-                NSString *title = hunt[@"title"];
-                NSString *subtitle = hunt[@"tagline"];
-                NSString *commentLink = [NSString stringWithFormat:@"http://www.producthunt.com%@", hunt[@"permalink"]];
+//            NSLog(@"response %@", [response description]);
 
-                NSDictionary *site = @{@"productLink":productLink,
-                                       @"title":title,
-                                       @"subtitle":subtitle,
-                                       @"commentLink":commentLink};
+            for (NSDictionary *article in response[@"list"])
+            {
+                NSDictionary *site = @{@"url":response[@"list"][[NSString stringWithFormat:@"%@",article]][@"resolved_url"],
+                                       @"title":response[@"list"][[NSString stringWithFormat:@"%@",article]][@"resolved_title"],
+                                       @"excerpt":response[@"list"][[NSString stringWithFormat:@"%@",article]][@"excerpt"],
+                                       @"item_id":response[@"list"][[NSString stringWithFormat:@"%@",article]][@"item_id"]};
 
                 [self.data addObject:site];
             }
+
+//            NSLog(@"data %@",self.data);
 
             [[NSNotificationCenter defaultCenter] postNotificationName:@"Pocket" object:self.data];
         }
         else
         {
+            NSLog(@"error %@", [error localizedDescription]);
+
             UIAlertView *alert = [[UIAlertView alloc] init];
             alert.title = @"Error Retrieving Data";
             alert.message = @"Please check your internet connection & for an app update (API might be broken)";
@@ -80,18 +79,25 @@
     }];
 }
 
-+ (NSDictionary *)layoutFrom:(NSDictionary *)post
++ (NSDictionary *)layoutFrom:(NSDictionary *)site
 {
-    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [self width:post], [self height:post])];
+    UIView *contentView = [[UIView alloc] initWithFrame:CGRectMake(0, 0, [self width:site], [self height:site])];
     contentView.backgroundColor = [UIColor whiteColor];
 
-    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(20, 0, 320-68-5, contentView.frame.size.height)];
+    UITextView *textView = [[UITextView alloc] initWithFrame:CGRectMake(20, 0, 300, contentView.frame.size.height)];
     UIView *borderView = [[UIView alloc] initWithFrame:CGRectMake(contentView.frame.origin.x+20,
                                                                   contentView.frame.size.height-.5,
                                                                   contentView.frame.size.width-20,
                                                                   .5)];
 
-    textView.text = [NSString stringWithFormat:@"%@\n%@",post[@"title"],post[@"subtitle"]];
+    NSURL *url = [NSURL URLWithString:site[@"url"]];
+    NSString *host = url.host;
+    if ([host hasPrefix:@"www."])
+    {
+        host = [host substringFromIndex:[@"www." length]];
+    }
+
+    textView.text = [NSString stringWithFormat:@"%@\n%@",site[@"title"],host];
     textView.font = [UIFont systemFontOfSize:13];
     textView.editable = NO;
     textView.selectable = NO;
@@ -103,12 +109,11 @@
     [contentView addSubview:borderView];
 
     return @{@"contentView":contentView};
-
 }
 
 + (NSString *)selected:(NSDictionary *)site
 {
-    return site[@"productLink"];
+    return site[@"url"];
 }
 
 + (CGFloat)width:(NSDictionary *)site
@@ -119,6 +124,27 @@
 + (CGFloat)height:(NSDictionary *)site
 {
     return 68;
+}
+
+- (void)deletePocket
+{
+    NSError *error;
+    NSArray *actions = @[@{ @"action": @"delete", @"item_id": @"456853615" }];
+    NSData *jsonData = [NSJSONSerialization dataWithJSONObject: actions
+                                                       options: kNilOptions
+                                                         error: &error];
+    NSString *jsonString = [[NSString alloc] initWithData:jsonData encoding: NSUTF8StringEncoding];
+
+    NSDictionary* argumentDictionary = @{@"actions":jsonString};
+
+    [[PocketAPI sharedAPI] callAPIMethod:@"send"
+                          withHTTPMethod:PocketAPIHTTPMethodPOST
+                               arguments:argumentDictionary
+                                 handler:^(PocketAPI *api, NSString *apiMethod, NSDictionary *response, NSError *error)
+    {
+        NSLog(@"response %@", [response description]);
+        NSLog(@"error %@", [error localizedDescription]);
+    }];
 }
 
 @end
